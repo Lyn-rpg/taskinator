22e6347f421360aa9f8f1e154dd029a98087057f
bhari eutsey <bharilynn@outlook.com>
2021-11-21 23:54:11 -0600

    code refractor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
@@ -9,10 +11,25 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
 
+   
+
+// create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    // create div  to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    // class name 
+    taskInfoE1.className = "task-info";
+    //add html context to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" +taskTypeInput + "</span>";
+
+listItemE1.appendChild(taskInfoE1);
+
+    listItemE1.textContent = taskNameInput;
     tasksToDoE1.appendChild(listItemE1);
 };
 
